f5f38327b716627bb0ffae4222c75f33fc9d29ee
Nataliia Poltavets <nataliia.poltavets@students.ittralee.ie>
2023-10-11 18:30:26 +0100

    Created a script using a Characther Controller method. Signed-off-by: Nataliia Poltavets <nataliia.poltavets@students.ittralee.ie>

--- MyUnityProject/Assets/Scripts/PlayerController.cs
+++ MyUnityProject/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,16 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     //public Rigidbody theRB;
     public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
 
     // Start is called before the first frame update
     void Start()
     {
         //theRB = GetComponent<Rigidbody>();
+        controller =  GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -23,5 +28,14 @@ public class PlayerController : MonoBehaviour
         {
             theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
         }*/
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
