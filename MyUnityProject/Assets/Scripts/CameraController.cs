da90370adcf5f5320cabf15b3ff2d317c3c4af3d
Nataliia Poltavets <nataliia.poltavets@students.ittralee.ie>
2023-10-11 22:37:39 +0100

    Moving with camera rotation.
    Signed-off-by: Nataliia Poltavets <nataliia.poltavets@students.ittralee.ie>

--- MyUnityProject/Assets/Scripts/CameraController.cs
+++ MyUnityProject/Assets/Scripts/CameraController.cs
@@ -9,7 +9,10 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
 
     public bool useOffsetValues;
+
     public float rotateSpeed;
+
+    public Transform pivot;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,25 +20,38 @@ public class CameraController : MonoBehaviour
         {
             offset = target.position - transform.position;
         }
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        //Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         //Get the X positipn of the mouse & rotate the target
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         target.Rotate(0, horizontal, 0);
+
+        //Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        target.Rotate(-vertical, 0, 0);
+        pivot.Rotate(-vertical, 0, 0);
 
         //Move the camera based on the current rotation of the target & the original offset
         float desiredYAngle = target.eulerAngles.y;
-        float desiredXAngle = target.eulerAngles.x;
+        float desiredXAngle = pivot.eulerAngles.x;
 
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = target.position - (rotation * offset);
 
-       // transform.position = target.position - offset;
+        // transform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y -.5f, transform.position.z);
+        }
+
         transform.LookAt(target);
 
     }
--- MyUnityProject/Assets/Scripts/PlayerController.cs
+++ MyUnityProject/Assets/Scripts/PlayerController.cs
@@ -29,7 +29,11 @@ public class PlayerController : MonoBehaviour
             theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
         }*/
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
 
         if (controller.isGrounded)
         {
