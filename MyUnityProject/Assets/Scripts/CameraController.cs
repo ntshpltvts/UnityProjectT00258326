f1e00cd0ce3150a83432aaa36859ac758df3f480
Nataliia Poltavets <nataliia.poltavets@students.ittralee.ie>
2023-10-12 09:22:03 +0100

    Stopped camera flip & Inverted camera
    Signed-off-by: Nataliia Poltavets <nataliia.poltavets@students.ittralee.ie>

--- MyUnityProject/Assets/Scripts/CameraController.cs
+++ MyUnityProject/Assets/Scripts/CameraController.cs
@@ -13,17 +13,20 @@ public class CameraController : MonoBehaviour
     public float rotateSpeed;
 
     public Transform pivot;
+    public float maxViewAngle;
+    public float minViewAngle;
+    public bool invertY;
     // Start is called before the first frame update
     void Start()
     {
         if(!useOffsetValues)
         {
-            offset = target.position - transform.position;
+            offset = target.position-transform.position;
         }
-        pivot.transform.position = target.transform.position;
-        pivot.transform.parent = target.transform;
+        pivot.transform.position=target.transform.position;
+        pivot.transform.parent=target.transform;
 
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 
@@ -36,20 +39,41 @@ public class CameraController : MonoBehaviour
 
         //Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        pivot.Rotate(-vertical, 0, 0);
+        //pivot.Rotate(-vertical, 0, 0);
+        if(invertY)
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
 
+        //Limit up/down camera rotation
+
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, pivot.rotation.eulerAngles.y, 0);
+        }
+        if(pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x<360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, pivot.rotation.eulerAngles.y, 0);
+        }
+          
+       
+    
         //Move the camera based on the current rotation of the target & the original offset
         float desiredYAngle = target.eulerAngles.y;
-        float desiredXAngle = pivot.eulerAngles.x;
+        float desiredXAngle = target.eulerAngles.x;
 
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
-        transform.position = target.position - (rotation * offset);
+        transform.position = target.position - (pivot.rotation * offset);
 
         // transform.position = target.position - offset;
 
         if(transform.position.y < target.position.y)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y -.5f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y -.3f, transform.position.z);
         }
 
         transform.LookAt(target);
